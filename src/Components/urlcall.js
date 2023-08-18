356f306aadc6165f217e0951111827c9d05c77be
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 <thanseermedammal720@gmail.com>
2023-08-18 16:50:52 +0530

    url changed

--- src/Components/urlcall.js
+++ src/Components/urlcall.js
@@ -1,3 +1,3 @@
 
-export const BaseURL = 'http://192.168.1.37:8000/'
-// export const BaseURL = 'https://iphonetest.foxiomlabs.com/'
+// export const BaseURL = 'http://192.168.1.37:8000/'
+export const BaseURL = 'https://backend.zellstores.com'
